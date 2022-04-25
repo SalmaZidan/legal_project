1a2f461cf32a50c49a70353237db867cc59fae88
SalmaZidan <salmazidan1997@gmail.com>
2022-04-25 13:56:05 +0200

    dashboard layouts

---
+++ resources/views/dashboard/layouts/scripts.blade.php
@@ -0,0 +1,21 @@
+  <!-- plugins:js -->
+  <script src="{{ URL::asset('assets/js/vendor.bundle.base.js') }}"></script>
+  <!-- endinject -->
+  <!-- Plugin js for this page -->
+  <script src="{{ URL::asset('assets/vendors/chart.js/Chart.min.js') }}"></script>
+  <script src="{{ URL::asset('assets/vendors/bootstrap-datepicker/bootstrap-datepicker.min.js') }}"></script>
+  <script src="{{ URL::asset('assets/vendors/progressbar.js/progressbar.min.js') }}"></script>
+
+  <!-- End plugin js for this page -->
+  <!-- inject:js -->
+  <script src="{{ URL::asset('assets/js/off-canvas.js') }}"></script>
+  <script src="{{ URL::asset('assets/js/hoverable-collapse.js') }}"></script>
+  <script src="{{ URL::asset('assets//template.js') }}"></script>
+  <script src="{{ URL::asset('assets/js/settings.js') }}"></script>
+  <script src="{{ URL::asset('assets/js/todolist.js') }}"></script>
+  <!-- endinject -->
+  <!-- Custom js for this page-->
+  <script src="{{ URL::asset('assets/js/jquery.cookie.js') }}" type="text/javascript"></script>
+  <script src="{{ URL::asset('assets/js/dashboard.js') }}"></script>
+  <script src="{{ URL::asset('assets/js/Chart.roundedBarCharts.js') }}"></script>
+  <!-- End custom js for this page-->
--- routes/web.php
+++ routes/web.php
@@ -12,10 +12,46 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+//Clear Cache facade value:
+Route::get('/clear-cache', function() {
+    $exitCode = Artisan::call('cache:clear');
+    return '<h1>Cache facade value cleared</h1>';
+});
+
+//Reoptimized class loader:
+Route::get('/optimize', function() {
+    $exitCode = Artisan::call('optimize');
+    return '<h1>Reoptimized class loader</h1>';
+});
+
+//Route cache:
+Route::get('/route-cache', function() {
+    $exitCode = Artisan::call('route:cache');
+    return '<h1>Routes cached</h1>';
+});
+
+//Clear Route cache:
+Route::get('/route-clear', function() {
+    $exitCode = Artisan::call('route:clear');
+    return '<h1>Route cache cleared</h1>';
+});
+
+//Clear View cache:
+Route::get('/view-clear', function() {
+    $exitCode = Artisan::call('view:clear');
+    return '<h1>View cache cleared</h1>';
+});
+
+//Clear Config cache:
+Route::get('/config-cache', function() {
+    $exitCode = Artisan::call('config:cache');
+    return '<h1>Clear Config cleared</h1>';
+});
+
 Route::group(['prefix' => LaravelLocalization::setLocale()], function()
 {
     Route::get('/', function () {
-        return view('welcome');
+        return view('dashboard.home');
     });
 });
 
